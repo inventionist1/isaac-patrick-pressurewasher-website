6f585117fd2c67eff2a704079570c2eae4755b91
RutRoRaggy <patrick22434@gmail.com>
2023-04-18 05:50:26 -0500

    uh nav bar?

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lan="en">
     <head>
-        <style>
-            :root {
-                --b: #1d1dbf;
-            }
-
-            .title {
-                color: var(--b);
-
-            }
-        </style>
+        <title> PressureWashing Company </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/reset.css">
     </head>
     <body>
-        <p class='title'> I Love NigAs</p>
+            <nav>
+                <ul class="nav-bar">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Signup</a></li>
+                </ul>
+            </nav>
+        <script src='js/script.js'> </script>
     <body>
 </html>
