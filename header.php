9174777c28921179251db3cfbe4f3970bdecc4d9
inventionist1 <42523031+inventionist1@users.noreply.github.com>
2023-04-19 00:31:47 -0500

    split index into 3 seperate files

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lan="en">
+    <head>
+        <title> PressureWashing Company </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/reset.css">
+    </head>
+    <body>
+            <nav>
+                <ul class="nav-bar">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="signup.php">Signup</a></li>
+                </ul>
+            </nav>
--- index.php
+++ index.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lan="en">
-    <head>
-        <title> PressureWashing Company </title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/reset.css">
-    </head>
-    <body>
-            <nav>
-                <ul class="nav-bar">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Signup</a></li>
-                </ul>
-            </nav>
+<?php
+    include_once 'header.php'
+?>
         <script src='js/script.js'> </script>
-    <body>
-</html>
+<?php
+    include_once 'footer.php'
+?>
