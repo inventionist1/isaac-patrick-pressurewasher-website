96e7f7ba95c7be0fddcb0ba43d90e4c5bfeac5f7
inventionist1 <42523031+inventionist1@users.noreply.github.com>
2023-04-20 09:42:16 -0500

    fixed signup.php

--- signup.php
+++ signup.php
@@ -16,33 +16,24 @@
         </div>
         <?php 
         if (isset($_GET["error"])) {
-            switch ($_GET["error"]) {
-                case "emptyinput":
-                    echo "<p>Please fill in all fields.</p>";
-                    break;
-                case "invaliduid":
-                    echo "<p>Usernames may only contain letters, numbers, and _.</p>";
-                    break;
-                case "invalidemail":
-                    echo "<p>Please enter a proper email!</p>";
-                    break;
-                case "pwddontmatch":
-                    echo "<p>Passwords don't match.</p>";
-                    break;
-                case "usernameoremailtaken":
-                    echo "<p>This username is taken.</p>";
-                    break;
-                case "stmtfailed":
-                    echo "<p>Something went wrong, Please try again later.</p>";
-                    break;
-                case "none":
-                    echo "<p>You have succesfully signed up.</p>";
-                    break;
-                default:
-                    echo "<p>An unknown error has occured. Please try again later.</p>";
-                    break;
-                }
+            if($_GET["error"] == "emptyinput") {
+                echo "<p>Please fill in all fields.</p>";
+            } elseif ($_GET["error"] == "invaliduid") {
+                echo "<p>Usernames may only contain letters, numbers, and _.</p>";
+            } elseif ($_GET["error"] == "invalidemail") {
+                echo "<p>Please enter a proper email.</p>";
+            } elseif ($_GET["error"] == "pwddontmatch") {
+                echo "<p>Passwords don't match.</p>";
+            } elseif ($_GET["error"] == "usernameoremailtaken") {
+                echo "<p>This username is taken.</p>";
+            } elseif ($_GET["error"] == "stmtfailed") {
+                echo "<p>Something went wrong, Please try again later.</p>";
+            } elseif ($_GET["error"] == "none") {
+                echo "<p>You have succesfully signed up.</p>";
+            } else {
+                echo "<p>An unknown error has occured. Please try again later.</p>";
             }
+        }
         ?>
     </section>
 
